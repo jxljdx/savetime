bf87e469e377b89ea74209b31a3a6172558c3710
Jundong Liao <jxljdx@gmail.com>
2015-01-16 21:14:57 -0800

    Made savetime item's title unique so no duplicate items can be added. Also add model migrations directories to gitignore.

--- savetimeapp/models.py
+++ savetimeapp/models.py
@@ -6,7 +6,7 @@ class Item(models.Model):
     """A save time item which contains basic info like title, link to the site
        detailing about the item, etc.
     """
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
     created_at = models.DateTimeField(default=timezone.now())
     num_likes = models.IntegerField(default=0)
     url = models.CharField(max_length=300)
