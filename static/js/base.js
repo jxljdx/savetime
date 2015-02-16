33801f7f39534e4e137403df0c8922e32265b38c
Jundong Liao <jxljdx@gmail.com>
2015-02-15 20:17:29 -0800

    New UI for search and categorization.

--- static/js/base.js
+++ static/js/base.js
@@ -1,3 +1,40 @@
+function isHomepage() {
+    var url = location.href;
+    var domain = location.origin;
+
+    // Delete anchor at the end of url, and remove slashes for both url
+    // and domain, check if the modified url is the same as modified
+    // domain, if they are, current page is the homepage.
+    url = url.substring(0, url.indexOf("#")).split("/").join("");
+    domain = domain.split("/").join("");
+    if (url == domain) {
+        return true;
+    }
+
+    return false;
+}
+
+$(document).ready(function() {
+    // Hack: align search button with search input box in Firefox
+    if (navigator.userAgent.indexOf("Firefox") != -1 ) {
+        $("div#search input[type=button]").css("top", "-2px");
+    }
+
+    $searchDiv = $("div#search");
+    $(".header-menu-item.search-item").click(function() {
+        $searchDiv.slideToggle();
+    });
+
+    // Only when the home page is requested, we show the search div by default
+    if (!isHomepage()) {
+        $searchDiv.css("display", "none");
+    }
+});
+
+/*
+ * [ Alternative entry UI for search and categorization ]
+ *
+
 var animated = false;
 
 $(document).ready(function() {
@@ -74,3 +111,5 @@ $(document).ready(function() {
         }
     });
 });
+
+*/
